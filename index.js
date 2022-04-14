72e19aecaa4e708d9ae361ad0a3dd9f5e169cb83
elzp <piechelzbieta@wp.pl>
2022-04-14 19:17:44 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 import './style.css';
+
+const onAndOffSwitch = document.getElementById('light-toggle');
+const bulb = document.querySelector('.bulb');
+
+onAndOffSwitch.addEventListener('change', () => {
+  if (onAndOffSwitch.checked) {
+    bulb.classList.add('bulb--on');
+  } else {
+    bulb.classList.remove('bulb--on');
+  }
+});
